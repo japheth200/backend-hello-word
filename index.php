1564763567d54237edb7401a624b818ce4f517d7
japheth <japhethsinon12@gmail.com>
2022-04-05 09:17:33 +0000

    hello world!

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>заданя--Hello word!</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header>
+    <img src="640px-PHP-logo.svg.png" alt="" class="logo">
+    <div  class="header">    
+        <p>Hello, word!<p>
+    </div>
+    </header>
+
+    <main>
+        <?php
+            echo 'Hello, word!';
+        ?>
+    </main>
+
+    <footer>
+        <div class="footer">
+            <div>
+            <p>синон жафет олу-воле 211-329</p>
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
